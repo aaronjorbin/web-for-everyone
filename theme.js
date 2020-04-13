5743ea98a82c0e62331ef1b697fb76098fe55d35
Aaron Jorbin <aaron@jorb.in>
2020-04-13 19:56:17 -0400

    presented version

--- theme.js
+++ theme.js
@@ -1,11 +1,16 @@
-export default {
+import { github } from '@code-surfer/themes';
 
-	font: '"Verlag Cond A", "Verlag Cond B"',
-	heading: {
-		fontWeight: 300,
-	},
-	pre: {
-		fontSize: '0.75em' 
-	},
-	googleFont: 'https://cloud.typography.com/7598912/612904/css/fonts.css',
-}
+github.fonts = {
+	body: '"Verlag Cond A", "Verlag Cond B"',
+	heading: '"Verlag Cond A", "Verlag Cond B"',
+};
+github.fontWeights = { body: 300, heading: 300, bold: 700 };
+github.colors = {
+	background: '#eee',
+	text: '#900',
+	primary: '#900',
+};
+github.googleFont = 'https://cloud.typography.com/7598912/612904/css/fonts.css';
+github.styles.CodeSurfer.subtitle.backgroundColor = '#900';
+
+export default github;
